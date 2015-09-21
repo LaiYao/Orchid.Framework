630dedc3fd8e9851d8ea074578674a1c5dc11bef
LaiYao <LaiYao@live.cn>
2015-09-21 17:46:12 +0800

    1. 忽略掉的代码提交

--- CoreFX/Orchid.Repo.EF/RepositoryEF.cs
+++ CoreFX/Orchid.Repo.EF/RepositoryEF.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
@@ -6,7 +7,7 @@ using Orchid.Repo.Contracts;
 
 namespace Orchid.Repo.EF
 {
-    public class RepositoryEF<T> : Repository<T> where T : class
+    public class RepositoryEF<T> : Repository<T> where T : class, new()
     {
         #region | Properties |
 
@@ -42,100 +43,58 @@ namespace Orchid.Repo.EF
             return _Context.Context.Set<T>().Any(cretiria);
         }
 
-        public override async Task<bool> AnyAsync(Expression<Func<T, bool>> cretiria)
-        {
-            return await Task<bool>.Factory.StartNew(() => _Context.Context.Set<T>().Any(cretiria));
-        }
+        public override async Task<bool> AnyAsync(Expression<Func<T, bool>> cretiria) => await Task.FromResult(Any(cretiria));
 
-        public new IQueryable<T> Find(Expression<Func<T, bool>> cretiria)
+        public override IEnumerable<T> Find(Expression<Func<T, bool>> cretiria)
         {
             return _Context.Context.Set<T>().Where(cretiria);
         }
 
-        public new async Task<IQueryable<T>> FindAsync(Expression<Func<T, bool>> cretiria)
-        {
-            return await Task<IQueryable<T>>.Factory.StartNew(() => _Context.Context.Set<T>().Where(cretiria));
-        }
+        public override async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> cretiria) 
+            => await Task.FromResult(Find(cretiria));
 
-        public new IQueryable<T> Find<TOrderKey>(Expression<Func<T, bool>> cretiria, Expression<Func<T, TOrderKey>> orderBy, int pageIndex, out int itemsCount, out int pagesCount, int countPerPage = 10)
+        public override PagingResult<T> Find<TOrderKey>(Expression<Func<T, bool>> cretiria, Expression<Func<T, TOrderKey>> orderBy, int pageIndex, int countPerPage = 10)
         {
             if (pageIndex < 0) throw new ArgumentOutOfRangeException(nameof(pageIndex));
 
             if (countPerPage < 1) throw new ArgumentOutOfRangeException(nameof(countPerPage));
 
-            itemsCount = _Context.Context.Set<T>().Where(cretiria).Count();
-
-            pagesCount = (int)Math.Ceiling((decimal)itemsCount / countPerPage);
-
-            return _Context.Context.Set<T>().Where(cretiria).OrderBy(orderBy).Skip(pageIndex * countPerPage).Take(countPerPage);
-        }
-
-        public new async Task<Tuple<IQueryable<T>, int, int>> FindAsync<TOrderKey>(Expression<Func<T, bool>> cretiria, Expression<Func<T, TOrderKey>> orderBy, int pageIndex, int countPerPage = 10)
-        {
-            if (pageIndex < 0) throw new ArgumentOutOfRangeException(nameof(pageIndex));
-
-            if (countPerPage < 1) throw new ArgumentOutOfRangeException(nameof(countPerPage));
-
-            var itemsCount = await Task<int>.Factory.StartNew(() => _Context.Context.Set<T>().Where(cretiria).Count());
+            var itemsCount = _Context.Context.Set<T>().Where(cretiria).Count();
 
             var pagesCount = (int)Math.Ceiling((decimal)itemsCount / countPerPage);
 
-            return await Task<Tuple<IQueryable<T>, int, int>>
-                .Factory
-                .StartNew(
-                () =>
-                new Tuple<IQueryable<T>, int, int>(
-                  _Context.Context.Set<T>().Where(cretiria).OrderBy(orderBy).Skip(pageIndex * countPerPage).Take(countPerPage),
-                  itemsCount,
-                  pagesCount));
+            var items = _Context.Context.Set<T>().Where(cretiria).OrderBy(orderBy).Skip(pageIndex * countPerPage).Take(countPerPage);
+
+            return new PagingResult<T>(items, itemsCount, pagesCount);
         }
 
-        public new IQueryable<T> FindAll()
+        public override async Task<PagingResult<T>> FindAsync<TOrderKey>(Expression<Func<T, bool>> cretiria, Expression<Func<T, TOrderKey>> orderBy, int pageIndex, int countPerPage = 10)
+            => await Task.FromResult(Find(cretiria, orderBy, pageIndex, countPerPage));
+
+        public override IEnumerable<T> FindAll()
         {
             return _Context.Context.Set<T>();
         }
 
-        public new async Task<IQueryable<T>> FindAllAsync()
-        {
-            return await Task<IQueryable<T>>
-                .Factory
-                .StartNew(
-                () =>
-                _Context.Context.Set<T>()
-                );
-        }
+        public override async Task<IEnumerable<T>> FindAllAsync() 
+            => await Task.FromResult(FindAll());
 
-        public new IQueryable<T> FindAll<TOrderKey>(Expression<Func<T, TOrderKey>> orderBy, int pageIndex, out int itemsCount, out int pagesCount, int countPerPage = 10)
+        public override PagingResult<T> FindAll<TOrderKey>(Expression<Func<T, TOrderKey>> orderBy, int pageIndex, int countPerPage = 10)
         {
             if (pageIndex < 0) throw new ArgumentOutOfRangeException(nameof(pageIndex));
 
             if (countPerPage < 1) throw new ArgumentOutOfRangeException(nameof(countPerPage));
 
-            itemsCount = _Context.Context.Set<T>().Count();
-
-            pagesCount = (int)Math.Ceiling((decimal)itemsCount / countPerPage);
-
-            return _Context.Context.Set<T>().OrderBy(orderBy).Skip(pageIndex * countPerPage).Take(countPerPage);
-        }
-
-        public new async Task<Tuple<IQueryable<T>, int, int>> FindAllAsync<TOrderKey>(Expression<Func<T, TOrderKey>> orderBy, int pageIndex, int countPerPage = 10)
-        {
-            if (pageIndex < 0) throw new ArgumentOutOfRangeException(nameof(pageIndex));
-
-            if (countPerPage < 1) throw new ArgumentOutOfRangeException(nameof(countPerPage));
-
-            var itemsCount = await Task<int>.Factory.StartNew(() => _Context.Context.Set<T>().Count());
+            var itemsCount = _Context.Context.Set<T>().Count();
 
             var pagesCount = (int)Math.Ceiling((decimal)itemsCount / countPerPage);
 
-            return await Task<Tuple<IQueryable<T>, int, int>>
-                .Factory
-                .StartNew(
-                () =>
-                new Tuple<IQueryable<T>, int, int>(
-                _Context.Context.Set<T>().OrderBy(orderBy).Skip(pageIndex * countPerPage).Take(countPerPage),
-                itemsCount,
-                pagesCount));
+            var items = _Context.Context.Set<T>().OrderBy(orderBy).Skip(pageIndex * countPerPage).Take(countPerPage);
+
+            return new PagingResult<T>(items, itemsCount, pagesCount);
         }
+
+        public override async Task<PagingResult<T>> FindAllAsync<TOrderKey>(Expression<Func<T, TOrderKey>> orderBy, int pageIndex, int countPerPage = 10)
+            => await Task.FromResult(FindAll(orderBy, pageIndex, countPerPage));
     }
 }
